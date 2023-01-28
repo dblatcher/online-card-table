58ae7205671a59be41a76b410e52953a0260219a
dblatcher <dv.bltchr@gmail.com>
2023-01-28 17:50:45 +0000

    remove Message box app

--- resources/js/board-game.ts
+++ resources/js/board-game.ts
@@ -2,7 +2,6 @@ import '../scss/boardGame.scss'
 import { h, render } from 'preact'
 import { openClientSocket } from './socket/client-socket'
 import { BoardGameApp } from './BoardGameApp'
-import { MessageBox } from './MessageBox'
 
 function initBoardGame () {
   const container = document.querySelector('#app-container')
@@ -19,14 +18,6 @@ function initBoardGame () {
     h(BoardGameApp, { socket, roomName }),
     container
   )
-
-  const messageBoxContainer = document.querySelector('.message-box')
-  if (messageBoxContainer) {
-    render(
-      h(MessageBox, { socket, roomName }),
-      messageBoxContainer
-    )
-  }
 }
 
 window.onload = () => {
